7a08d05e569c4d27fcfbe76ee4c62a1007f1cfe6
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-08-17 18:09:33 +0100

    food

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -14,9 +14,9 @@ const Search = () => {
         color: '#8c101f'
     }
     let divStyle = {
-        position: 'absolute',
-        // width: '100%',
-        // height: '100%',
+        // position: 'absolute',
+        width: '100vw',
+        height: '100vh',
         backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)),
         url(${image2})`,
         backgroundSize: 'cover',
@@ -61,7 +61,7 @@ const Search = () => {
 
   return (
     <>
-        <div style={divStyle} className='pt-lg-5 bg-local'>
+        <div style={divStyle} className='pt-lg-5 bg-fixed h-screen w-screen'>
 
         <section className='container-fluid'>
             <div className="row">
@@ -71,9 +71,7 @@ const Search = () => {
                     <button className='w-100 btn' style={bgWine} onClick={searchFood}>Search Food</button>
                 </div>
                 <div className='col-lg-6 col-md-8 col-sm-12 rounded-3'>
-                    {/* <p>
-                        Lorem,i ipsum dololr sit, amllet consecte;tur adipisicing eklit. Dolor aperiam nemo neque ut autem. Nobis, aspernatur magni molestiae quo mollitia ipsam perferendis maxime dignissimos. Nemo, ullam aliquam quod odit deleniti eius sed magni, autem dolorum debitis aliquid nostrum culpa. Maxime doloribus laboriosam, reprehenderit odit necessitatibus cum distinctio possimus perferendis vel!
-                    </p> */}
+                   
                 </div>
             </div>
         </section>
@@ -83,14 +81,14 @@ const Search = () => {
                     <div className='row mt-3'>
                     <div style={bgstyle} className=' col-lg-6 col-md-8 col-sm-12 rounded-3'>
                     <div key={index}>
-                        <h1 className='fw-bold' style={myColor}>{item.strMeal}</h1>
-                        <h3 className='fw-bold'>Location: {item.strArea}</h3>
-                        <h4>Ingredients: {item.strIngredient1} {item.strIngredient2} {item.strIngredient3} {item.strIngredient4} {item.strIngredient5}  {item.strIngredient6}  {item.strIngredient7}  {item.strIngredient8}  {item.strIngredient9}</h4>
-                        <p>{item.strInstructions}</p>
+                        <h1 className='font-bold text-3xl mt-2' style={myColor}>{item.strMeal}</h1>
+                        <h3 className='font-bold text-3xl mt-2'>Location: {item.strArea}</h3>
+                        <h4><span className='text-2xl font-medium'>Ingredients:</span> {item.strIngredient1} {item.strIngredient2} {item.strIngredient3} {item.strIngredient4} {item.strIngredient5}  {item.strIngredient6}  {item.strIngredient7}  {item.strIngredient8}  {item.strIngredient9}</h4>
+                        <p className='text-justify leading-6'><span className='text-2xl font-medium'>How to make:</span> {item.strInstructions}</p>
                     </div>
                 </div>
                 <div className='col-lg-6 col-md-8 col-sm-12 rounded-3'>
-                    <img className='rounded-3 img-fluid w-100 h-75' src={item.strMealThumb} alt="" />
+                    <img className='rounded-3 img-fluid w-100' src={item.strMealThumb} alt="" />
                 </div>
 
             </div>
